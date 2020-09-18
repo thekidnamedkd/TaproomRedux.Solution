1c80e29142dfa10a36dba73fdc5eea4a280186e4
Kevin Davis <thekidnamedkd@gmail.com>
2020-09-18 16:06:49 -0700

    fail test for delete keg

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -2,6 +2,25 @@ import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
 
+  const currentState = {
+    1: {
+      kegName: "Kevin's Ale",
+      kegBrand: "Double Davis Mountain",
+      kegPrice: 7,
+      kegABV: 7.5,
+      kegAmt: 124,
+      kegId: 1 
+    },
+    2: {
+      kegName: "Kevin's Lager",
+      kegBrand: "Fort Davis",
+      kegPrice: 6,
+      kegABV: 5.2,
+      kegAmt: 124,
+      kegId: 2
+    }
+  };
+
   let action;
   const kegData = {
     kegName: "Kevin's Ale",
@@ -28,16 +47,32 @@ describe('kegListReducer', () => {
       kegId: kegId
     };
 
-  expect(kegListReducer({}, action)).toEqual({
-    [kegId]: {
-      kegName: kegName,
-      kegBrand: kegBrand,
-      kegPrice: kegPrice,
-      kegABV: kegABV,
-      kegAmt: kegAmt,
-      kegId: kegId
-    }
-  });
+    expect(kegListReducer({}, action)).toEqual({
+      [kegId]: {
+        kegName: kegName,
+        kegBrand: kegBrand,
+        kegPrice: kegPrice,
+        kegABV: kegABV,
+        kegAmt: kegAmt,
+        kegId: kegId
+      }
+    });
   });
 
+  test('Should successfully delete a keg', () => {
+    action = {
+      type: 'DELETE_TICKET',
+      kegId: 1
+    };
+    expect(kegListReducer(currentState, action)).toEqual({
+      2: {
+        kegName: "Kevin's Lager",
+        kegBrand: "Fort Davis",
+        kegPrice: 6,
+        kegABV: 5.2,
+        kegAmt: 124,
+        kegId: 2
+      }
+    });
+  });
 });
