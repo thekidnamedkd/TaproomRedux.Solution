73515b5e0a1e401cb8150a3e7f70e131dd48f758
Kevin Davis <thekidnamedkd@gmail.com>
2020-09-18 15:04:58 -0700

    pass test for keg data

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
+  const { kegName, kegBrand, kegPrice, kegABV, kegAmt, kegId } = action;
+  switch (action.type) {
+    case 'ADD_KEG':
+      return Object.assign({}, state, {
+        [kegId]: {
+          kegName: kegName,
+          kegBrand: kegBrand,
+          kegPrice: kegPrice,
+          kegABV: kegABV,
+          kegAmt: kegAmt,
+          kegId: kegId
+        }
+      });
+  default:
     return state;
   }
+};
